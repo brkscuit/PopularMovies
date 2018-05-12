90301541b491d00ae5d7fb8d5a60d8caf53a57aa
brkscuit <brookscuit@gmail.com>
2018-05-12 19:59:41 -0400

    Download JSON from Movie sight

---
+++ app/src/main/java/com/stitchycoder/popularmovies/Movie.java
@@ -0,0 +1,72 @@
+package com.stitchycoder.popularmovies;
+
+import android.media.Image;
+
+import java.util.Date;
+
+/**
+ * Created by brook on 5/9/2018.
+ */
+
+public class Movie {
+
+    private int mMovieId;
+    private String mTitle;
+    private String mPosterPath;
+    private String mOverview;
+    private double mUserRating;
+    private String mReleaseDate;
+
+    public Movie(int movieId) {
+        this.mMovieId = movieId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+
+    public String getOverview() {
+        return mOverview;
+    }
+
+    public void setOverview(String overview) {
+        mOverview = overview;
+    }
+
+    public int getMovieId() {
+        return mMovieId;
+    }
+
+    public void setMovieId(int movieId) {
+        mMovieId = movieId;
+    }
+
+    public String getPosterPath() {
+        return mPosterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        mPosterPath = posterPath;
+    }
+
+    public double getUserRating() {
+        return mUserRating;
+    }
+
+    public void setUserRating(double userRating) {
+        mUserRating = userRating;
+    }
+
+    public String getReleaseDate() {
+        return mReleaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        mReleaseDate = releaseDate;
+    }
+}
